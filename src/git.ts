df8ba1a00eeda77e6a9a51e8c8bb5fd0d992a94f
Kyle McKee <kmckee@itscovered.com>
2025-02-26 09:05:28 -0500

    extract git stuff from main

---
+++ src/git.ts
@@ -0,0 +1,94 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import { writeFile, readFile } from 'fs/promises'
+
+export const execOrThrow: (
+  ...args: Parameters<typeof exec.exec>
+) => Promise<void> = async (...args) => {
+  core.info(`Executing command: ${args[0]} ${args[1]?.join(' ') ?? ''}`)
+  const exitCode = await exec.exec(...args)
+  if (exitCode !== 0)
+    throw Error(`error running command: ${args[0]} ${args[1]?.join(' ') ?? ''}`)
+}
+
+export const setToken = async (token: string) => {
+  core.info('Setting GitHub token')
+  const encodedToken = Buffer.from(`x-access-token:${token}`, 'utf-8').toString(
+    'base64'
+  )
+  core.setSecret(encodedToken)
+  const headerPlaceholder = 'Authorization: basic ***'
+  const headerValue = `Authorization: basic ${encodedToken}`
+  const headerKey = 'http.https://github.com/.extraheader'
+  const configPath = '.git/config'
+
+  await execOrThrow('git', ['config', '--local', headerKey, headerPlaceholder])
+  const configString = await readFile(configPath, 'utf-8')
+  await writeFile(
+    configPath,
+    configString.replace(headerPlaceholder, headerValue)
+  )
+
+  return () =>
+    execOrThrow('git', ['config', '--local', '--unset-all', headerKey])
+}
+
+export const checkoutBranch = async (branch: string) => {
+  core.info(`Checking out branch: ${branch}`)
+  try {
+    await execOrThrow('git', [
+      'ls-remote',
+      '--heads',
+      '--exit-code',
+      'origin',
+      branch
+    ])
+    await execOrThrow('git', ['fetch', '-u', 'origin', `${branch}:${branch}`])
+    await execOrThrow('git', ['symbolic-ref', 'HEAD', `refs/heads/${branch}`])
+  } catch {
+    await execOrThrow('git', ['checkout', '-b', branch])
+  }
+}
+
+export const stagedChangesExist = async () => {
+  const exitCode = await exec.exec(
+    'git',
+    ['diff-index', '--quiet', '--cached', 'HEAD', '--'],
+    { ignoreReturnCode: true }
+  )
+
+  return exitCode !== 0
+}
+
+export const commitAndPush = async (targetBranch: string, force: boolean) => {
+  core.info('Committing changes...')
+  await execOrThrow('git', ['add', '.'])
+  if ((await stagedChangesExist()) == false) {
+    core.info('No changes detected, skipping commit...')
+  } else {
+    await execOrThrow('git', ['config', 'user.name', 'Mintie Bot'])
+    await execOrThrow('git', ['config', 'user.email', 'aws@mintlify.com'])
+    await execOrThrow('git', ['commit', '-m', 'update'])
+
+    const pushArgs = ['push']
+    if (force) pushArgs.push('--force')
+    pushArgs.push('origin', targetBranch)
+    core.info('Pushing changes...')
+    await execOrThrow('git', pushArgs)
+  }
+}
+
+export const clone = async (
+  token: string,
+  owner: string,
+  repo: string,
+  branch: string = 'main'
+) => {
+  const args = ['clone', '--depth=1']
+  if (branch) args.push('--branch', branch)
+  const url = `https://oauth2:${token}@github.com/${owner}/${repo}`
+  args.push(url)
+  core.info(`url is: ${url}`)
+
+  await execOrThrow('git', args)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,17 @@
 import * as core from '@actions/core'
-import * as exec from '@actions/exec'
 import * as io from '@actions/io'
 import { writeFile, readFile } from 'fs/promises'
 import { parse } from 'yaml'
 import type { MintConfig } from '@mintlify/models'
 import path from 'path'
 import mergeConfigs from './mergeConfigs.js'
+import * as git from './git.js'
 
 type Repo = {
   owner: string
   repo: string
   ref?: string
   subdirectory?: string
-}
-
-const execOrThrow: (
-  ...args: Parameters<typeof exec.exec>
-) => Promise<void> = async (...args) => {
-  core.info(`Executing command: ${args[0]} ${args[1]?.join(' ') ?? ''}`)
-  const exitCode = await exec.exec(...args)
-  if (exitCode !== 0)
-    throw Error(`error running command: ${args[0]} ${args[1]?.join(' ') ?? ''}`)
-}
-
-const setToken = async (token: string) => {
-  core.info('Setting GitHub token')
-  const encodedToken = Buffer.from(`x-access-token:${token}`, 'utf-8').toString(
-    'base64'
-  )
-  core.setSecret(encodedToken)
-  const headerPlaceholder = 'Authorization: basic ***'
-  const headerValue = `Authorization: basic ${encodedToken}`
-  const headerKey = 'http.https://github.com/.extraheader'
-  const configPath = '.git/config'
-
-  await execOrThrow('git', ['config', '--local', headerKey, headerPlaceholder])
-  const configString = await readFile(configPath, 'utf-8')
-  await writeFile(
-    configPath,
-    configString.replace(headerPlaceholder, headerValue)
-  )
-
-  return () =>
-    execOrThrow('git', ['config', '--local', '--unset-all', headerKey])
-}
-
-const checkoutBranch = async (branch: string) => {
-  core.info(`Checking out branch: ${branch}`)
-  try {
-    await execOrThrow('git', [
-      'ls-remote',
-      '--heads',
-      '--exit-code',
-      'origin',
-      branch
-    ])
-    await execOrThrow('git', ['fetch', '-u', 'origin', `${branch}:${branch}`])
-    await execOrThrow('git', ['symbolic-ref', 'HEAD', `refs/heads/${branch}`])
-  } catch {
-    await execOrThrow('git', ['checkout', '-b', branch])
-  }
 }
 
 /**
@@ -80,14 +32,14 @@ export async function run(): Promise<void> {
       core.info(`Changing directory to: ${subdirectory}`)
       process.chdir(subdirectory)
 
-      await checkoutBranch(targetBranch)
+      await git.checkoutBranch(targetBranch)
 
       const mainConfig = JSON.parse(
         await readFile('docs.json', 'utf-8')
       ) as MintConfig
 
       let wipConfig = mainConfig
-      resetToken = await setToken(token)
+      resetToken = await git.setToken(token)
       for (const {
         owner,
         repo,
@@ -97,13 +49,7 @@ export async function run(): Promise<void> {
         core.info(`Processing repository: ${owner}/${repo}`)
         await io.rmRF(repo)
 
-        const args = ['clone', '--depth=1']
-        if (ref) args.push('--branch', ref)
-        const url = `https://oauth2:${token}@github.com/${owner}/${repo}`
-        args.push(url)
-        core.info(`url is: ${url}`)
-
-        await execOrThrow('git', args)
+        git.clone(token, owner, repo, ref)
 
         if (subrepoSubdirectory) {
           core.info(`Looking in subrepoSubdirectory: ${subrepoSubdirectory} `)
@@ -127,37 +73,7 @@ export async function run(): Promise<void> {
       core.info('Writing updated docs.json')
       await writeFile('docs.json', JSON.stringify(wipConfig, null, 2))
 
-      core.info('Committing changes...')
-      await execOrThrow('git', ['add', '.'])
-      try {
-        ;(await exec.exec('git', [
-          'diff-index',
-          '--quiet',
-          '--cached',
-          'HEAD',
-          '--'
-        ])) !== 0
-        core.info('No changes detected, skipping commit...')
-      } catch {
-        await execOrThrow('git', ['config', 'user.name', 'Mintie Bot'])
-        await execOrThrow('git', ['config', 'user.email', 'aws@mintlify.com'])
-        await execOrThrow('git', ['commit', '-m', 'update'])
-
-        const pushArgs = ['push']
-        if (force) pushArgs.push('--force')
-        pushArgs.push('origin', targetBranch)
-        core.info('Pushing changes...')
-        await execOrThrow('git', pushArgs)
-      }
-    } catch (error) {
-      const message =
-        error &&
-        typeof error === 'object' &&
-        'message' in error &&
-        typeof error.message === 'string'
-          ? error.message
-          : JSON.stringify(error, null, 2)
-      core.setFailed(message)
+      git.commitAndPush(targetBranch, force)
     } finally {
       resetToken?.()
     }
